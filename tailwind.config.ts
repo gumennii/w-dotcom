6e32241f890508dc646ca13e21e839bc51ad6f70
Yevhen Kolesnikov <yevhen.code@gmail.com>
2023-12-15 10:14:30 +0200

    update theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,22 +32,22 @@ export default {
           "accent-content":
             "#ffffff" /* Foreground content color to use on accent color */,
 
-          neutral: "#6B6B6B" /* Neutral color */,
-          "neutral-focus": "#6B6B6B" /* Neutral color - focused */,
+          neutral: "#101010" /* Neutral color */,
+          "neutral-focus": "#61636B" /* Neutral color - focused */,
           "neutral-content":
             "#ffffff" /* Foreground content color to use on neutral color */,
 
-          // "base-100":
-          //   "#ffffff" /* Base color of page, used for blank backgrounds */,
-          // "base-200": "#f9fafb" /* Base color, a little darker */,
+          "base-100":
+            "#ffffff" /* Base color of page, used for blank backgrounds */,
+          "base-200": "#6B6B6B" /* Base color, a little darker */,
           // "base-300": "#d1d5db" /* Base color, even more darker */,
           // "base-content":
           //   "#1f2937" /* Foreground content color to use on base color */,
 
-          info: "#3d7eeb" /* Info */,
-          success: "#09cc60" /* Success */,
-          warning: "#f7ac0d" /* Warning */,
-          error: "#d61f1f" /* Error */,
+          info: "#012F85" /* Info */,
+          success: "#24A840" /* Success */,
+          warning: "#FFB144" /* Warning */,
+          error: "#DC4130" /* Error */,
 
           "--rounded-btn": ".5rem",
 
