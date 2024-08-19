9280296afe6d23e5f4298d7953089ef28d6ac8bf
Mihail Gumennii <mihail.gumennii@gmail.com>
2024-08-19 16:36:35 -0500

    feat: pass uuid and creatd at to the leads

--- src/app/api/lead/route.ts
+++ src/app/api/lead/route.ts
@@ -1,18 +1,19 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
-import { format } from "date-fns";
+import { v4 as uuidv4 } from "uuid";
 
 export async function POST(request: Request) {
   const body = await request.json();
   const { first_name, email, items, page_slug, clinic_name } = body;
   const participants = JSON.stringify(items);
-  const dateCreation = format(new Date(), "MMM d, y");
+  const created_at = new Date().toISOString();
+  const id = uuidv4();
 
   try {
     if (!first_name || !email) throw new Error("Name and Email required");
     await sql`
-      INSERT INTO leads (Name, Email, clinic_name, page_slug, Participants, Date)
-      VALUES (${first_name}, ${email}, ${clinic_name}, ${page_slug}, ${participants}, ${dateCreation})
+      INSERT INTO leads (Name, Email, clinic_name, page_slug, Participants, id, created_at)
+      VALUES (${first_name}, ${email}, ${clinic_name}, ${page_slug}, ${participants}, ${id}, ${created_at})
       ON CONFLICT (Email) DO NOTHING;
     `;
     return NextResponse.json({ status: 200 });
