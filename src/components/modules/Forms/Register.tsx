89c07db248a20afebf6a0f84ba54657498efd7ae
Calvin Chan <calvin@nextlevelsports.com>
2024-08-22 08:02:33 -0700

    Removed xs as an option

--- src/components/modules/Forms/Register.tsx
+++ src/components/modules/Forms/Register.tsx
@@ -448,7 +448,6 @@ export const Register = ({ pageSlug, pageName }: RegisterProps) => {
                                   error={fieldState.error?.message}
                                 >
                                   <SelectOption value="">Select a shirt size</SelectOption>
-                                  <SelectOption value="YXS">Youth Extra Small</SelectOption>
                                   <SelectOption value="YS">Youth Small</SelectOption>
                                   <SelectOption value="YM">Youth Medium</SelectOption>
                                   <SelectOption value="YL">Youth Large</SelectOption>
